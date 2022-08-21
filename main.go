e85a298bffe2d57a6c1b52a4c430087fdb0b6caa
morry48 <yyyyuuuuuuuu0408@gmail.com>
2022-08-21 13:42:08 +0900

    ウェブサーバ

--- main.go
+++ main.go
@@ -12,8 +12,5 @@ func main() {
 	utils.LoggingSettings(config.Config.LogFile)
 	fmt.Println(models.DbConnection)
 	controllers.StreamIngestionData()
-	//apiClient := bitflyer.New(config.Config.ApiKey, config.Config.ApiSecret)
-
-	//tickerChannel := make(chan bitflyer.Ticker)
-	//apiClient.GetRealTimeTicker(config.Config.ProductCode, tickerChannel)
+	controllers.StartWebServer()
 }
